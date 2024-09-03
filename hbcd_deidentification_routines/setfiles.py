ceaa0d9bd24880a25ae230cc66d02bf1f34fc202
Erik Lee <leex6144@umn.edu>
2024-09-03 14:47:42 -0500

    [dev] another concat attempt

--- hbcd_deidentification_routines/setfiles.py
+++ hbcd_deidentification_routines/setfiles.py
@@ -47,11 +47,14 @@ def replace_in_strings(data, old_pattern, new_pattern):
 
     # If the data is a NumPy array, handle both structured and unstructured arrays
     elif isinstance(data, np.ndarray):
-        new_ndarray = np.empty(data.shape, dtype='U100')
-        for i in range(len(data)):
-            new_ndarray[i] = replace_in_strings(data[i], old_pattern, new_pattern)
-        return new_ndarray
-            #data[i] = replace_in_strings(data[i], old_pattern, new_pattern)
+        if data[0].str.startswith('<U'):
+            new_ndarray = np.empty(data.shape, dtype='U100')
+            for i in range(len(data)):
+                new_ndarray[i] = replace_in_strings(data[i], old_pattern, new_pattern)
+            return new_ndarray
+        else:
+            for i in range(len(data)):
+                data[i] = replace_in_strings(data[i], old_pattern, new_pattern)
 
     # If the data is a numpy.void object (e.g., a record in a structured array), handle its fields
     elif isinstance(data, np.void):
