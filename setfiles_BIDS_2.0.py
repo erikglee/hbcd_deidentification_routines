66f972d2f0899a56f9409b82b842dd1bd0453ed0
bilas003 <132010787+bilas003@users.noreply.github.com>
2024-10-10 21:58:09 -0500

    Update setfiles_BIDS_2.0.py

--- setfiles_BIDS_2.0.py
+++ setfiles_BIDS_2.0.py
@@ -14,7 +14,6 @@ import argparse
 #can be run from the command line using the arguments denoted in my_parser
 
 
-
 def replace_in_strings(data, old_pattern, new_pattern):
     """
     Recursively traverses a data structure to find strings and replace a pattern with a new one.
@@ -26,55 +25,115 @@ def replace_in_strings(data, old_pattern, new_pattern):
     
     Returns:
     - The modified data structure with the pattern replaced in strings.
+    - A boolean indicating whether the data structure was modified.
     """
+
+    minimum_pattern_length = len(old_pattern)
+
     # If the data is a dictionary, recursively apply the function to each value
     if isinstance(data, dict):
         for key, value in data.items():
-            data[key] = replace_in_strings(value, old_pattern, new_pattern)
+            change_needed, new_string = replace_in_strings(value, old_pattern, new_pattern)
+            if change_needed:
+                data[key] = new_string
 
     # If the data is a list or tuple, recursively apply the function to each element
     elif isinstance(data, list):
         for i in range(len(data)):
-            data[i] = replace_in_strings(data[i], old_pattern, new_pattern)
+            change_needed, new_string = replace_in_strings(data[i], old_pattern, new_pattern)
+            if change_needed:
+                data[i] = new_string
     elif isinstance(data, tuple):
-        data = tuple(replace_in_strings(list(data), old_pattern, new_pattern))
+        change_needed, new_string = replace_in_strings(list(data), old_pattern, new_pattern)
+        if change_needed:
+            data = tuple(new_string)
 
     # If the data is a NumPy array, handle both structured and unstructured arrays
     elif isinstance(data, np.ndarray):
         if data.dtype.str.startswith('<U'):
-            new_ndarray = np.empty(data.shape, dtype='U100')
-            for i in range(len(data)):
-                new_ndarray[i] = replace_in_strings(data[i], old_pattern, new_pattern)
-            return new_ndarray
+            if int(data.dtype.str.split('U')[1]) < minimum_pattern_length:
+                change_ever_needed = False
+                for i in range(len(data)):
+                    change_needed, new_string = replace_in_strings(data[i], old_pattern, new_pattern)
+                    if change_needed:
+                        data[i] = new_string
+                        change_ever_needed = True
+                return change_ever_needed, data
+            else:
+                print('Temporarily changing numpy array with shape {} and dtype {} to <U300'.format(data.shape, data.dtype.str))
+                new_ndarray = np.empty(data.shape, dtype='<U300')
+                change_ever_needed = False
+                maximum_observed_length = 0
+                for i in range(len(data)):
+                    change_needed, new_string = replace_in_strings(data[i], old_pattern, new_pattern)
+                    if len(new_string) > maximum_observed_length:
+                        maximum_observed_length = len(new_string)
+                    if change_needed:
+                        new_ndarray[i] = new_string
+                        change_ever_needed = True
+                if change_ever_needed:
+                    new_ndarray = new_ndarray.astype('<U{}'.format(maximum_observed_length))
+                    data = new_ndarray
+                    print('   final type: {}'.format(data.dtype.str))
+                return change_ever_needed, data
         else:
+            change_ever_needed = False
             for i in range(len(data)):
-                data[i] = replace_in_strings(data[i], old_pattern, new_pattern)
+                change_needed, new_string = replace_in_strings(data[i], old_pattern, new_pattern)
+                if change_needed:
+                    data[i] = new_string
+                    change_ever_needed = True
+            return change_ever_needed, data
 
     # If the data is a numpy.void object (e.g., a record in a structured array), handle its fields
     elif isinstance(data, np.void):
+        change_ever_needed = False
         for field_name in data.dtype.names:
-            data[field_name] = replace_in_strings(data[field_name], old_pattern, new_pattern)
+            change_needed, new_string = replace_in_strings(data[field_name], old_pattern, new_pattern)
+            if change_needed:
+                data[field_name] = new_string
+                change_ever_needed = True
+        return change_ever_needed, data
 
     # If the data is a string, replace the old pattern with the new one
     elif isinstance(data, str):
-        return data.replace(old_pattern, new_pattern)
+        #print('Potentially String.')
+        new_string = data.replace(old_pattern, new_pattern)
+        if new_string != data:
+            return True, new_string
+        else:
+            return False, data
 
     # If the data is a bytes object, replace the old pattern with the new one
     elif isinstance(data, bytes):
-        return data.replace(old_pattern.encode(), new_pattern.encode())
+        #print('Potentially changing bytes string.')
+        new_string = data.replace(old_pattern.encode(), new_pattern.encode())
+        if new_string != data:
+            return True, new_string
+        else:
+            return False, data
 
     # If the data is a NumPy string, replace the old pattern with the new one
     elif isinstance(data, np.str_):
-        return np.str_(data.replace(old_pattern, new_pattern), dtype='U100')
+        #print('Potentially changing numpy string.')
+        new_string = data.replace(old_pattern, new_pattern)
+        if new_string != data:
+            return True, new_string
+        else:
+            return False, data
 
     # If the data is a NumPy bytes string, replace the old pattern with the new one
     elif isinstance(data, np.bytes_):
-        return np.bytes_(data.replace(old_pattern.encode(), new_pattern.encode()))
+        #print('Potentially changing encoded bytes string.')
+        new_string = np.bytes_(data.replace(old_pattern.encode(), new_pattern.encode()))
+        if new_string != data:
+            return True, new_string
+        else:
+            return False, data
 
     # For any other data types, return the data as is
     #print(type(data))
-    return data
-
+    return False, data
 
 
 def replace_text_in_set_file(input_path_to_set_file, output_path_to_set_file, DCCID, PSCID, GUID):
